86ccaf78ac11e50724893b8dd6c7fa0f2eb0e0aa
Harpalsinh Jadeja <jadejaharpal14@gmail.com>
2024-04-29 18:02:41 +0530

    feat: return CORS headers

--- packages/react-app/next.config.js
+++ packages/react-app/next.config.js
@@ -1,13 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  webpack: (config) => {
-    config.resolve.fallback = {
-      fs: false,
-    };
-    return config;
-  },
+    reactStrictMode: true,
+    swcMinify: true,
+    webpack: (config) => {
+        config.resolve.fallback = {
+            fs: false,
+        };
+        return config;
+    },
+    async headers() {
+        return [
+            {
+                source: "/api/:path*",
+                headers: [
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: "*", // Set your origin
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
