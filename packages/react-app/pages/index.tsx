ed19f3c08b9a5d3f83780958c07afe26d512687b
Harpalsinh Jadeja <jadejaharpal14@gmail.com>
2024-04-29 16:05:52 +0530

    feat: api to lookup minipay identifier

---
+++ packages/react-app/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <main className="w-full flex justify-center text-gray-800 text-lg font-bold">
+            API to lookup MiniPay identifiers
+        </main>
+    );
+}
